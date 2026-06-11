7c9c8af443eff66e8c2ed13eac8d96f0d639e2e0
Ravindu Tharuka Weerasinghe <92865262+rtweera@users.noreply.github.com>
2026-06-11 21:36:39 +0530

    feat: update abstract Ui Steps support class by adding getters for PlantsPage, AddPlantPage, and SalesPage

--- src/test/java/stepdefinitions/UiStepSupport.java
+++ src/test/java/stepdefinitions/UiStepSupport.java
@@ -1,9 +1,12 @@
 package stepdefinitions;
 
 import com.microsoft.playwright.Page;
+import pages.AddPlantPage;
 import pages.CategoriesPage;
 import pages.DashboardPage;
 import pages.LoginPage;
+import pages.PlantsPage;
+import pages.SalesPage;
 import utils.PlaywrightFactory;
 
 abstract class UiStepSupport {
@@ -21,5 +24,17 @@ abstract class UiStepSupport {
 
     protected CategoriesPage categoriesPage() {
         return new CategoriesPage(page());
+    }
+
+    protected PlantsPage plantsPage() {
+        return new PlantsPage(page());
+    }
+
+    protected AddPlantPage addPlantPage() {
+        return new AddPlantPage(page());
+    }
+
+    protected SalesPage salesPage() {
+        return new SalesPage(page());
     }
 }
