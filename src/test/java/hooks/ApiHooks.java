b677d0919193c1b5fdf2808602c2a834349e634f
Ravindu Tharuka Weerasinghe <92865262+rtweera@users.noreply.github.com>
2026-06-12 19:07:15 +0530

    refactor: remove PlaywrightApiContext references from ApiHooks

--- src/test/java/hooks/ApiHooks.java
+++ src/test/java/hooks/ApiHooks.java
@@ -1,6 +1,5 @@
 package hooks;
 
-import api.PlaywrightApiContext;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -12,12 +11,10 @@ public class ApiHooks {
     public void initApiContext(Scenario scenario) {
         System.out.println("\n=== STARTING API: " + scenario.getName() + " ===");
         ApiTestContext.initContext();
-        PlaywrightApiContext.request = ApiTestContext.context().api;
     }
 
     @After(value = "@API", order = 0)
     public void destroyApiContext(Scenario scenario) {
-        PlaywrightApiContext.request = null;
         ApiTestContext.destroyContext();
         System.out.println("=== DONE API: " + scenario.getName() + " ===\n");
     }
